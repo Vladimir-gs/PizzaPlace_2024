d655883dfc6434d4a34cc0e4423aeac7302f8257
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-01-31 14:37:30 -0400

    Agregamos nuestra clase menu

---
+++ PizzaPlace_2024/Models/Menu.cs
@@ -0,0 +1,14 @@
+﻿namespace PizzaPlace_2024.Models
+{
+    public class Menu
+    {
+        public List<Pizza> Pizzas { get; set; }
+           = new List<Pizza>();
+
+        public void Add(Pizza pizza)
+            => Pizzas.Add(pizza);
+
+        public Pizza? GetPizza(int id)
+            => Pizzas.SingleOrDefault(pizza => pizza.Id == id);
+    }
+}
