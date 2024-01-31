03ce79c2a431e6fdf03f3dff0f64ffb997b5af39
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-01-31 14:41:56 -0400

    Agregamos requrimientos en la clase clientes

--- PizzaPlace_2024/Models/Clientes.cs
+++ PizzaPlace_2024/Models/Clientes.cs
@@ -1,10 +1,15 @@
-﻿namespace PizzaPlace_2024.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaPlace_2024.Models
 {
     public class Clientes
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Por favor proporcionar un Nombre.")]
         public string Nombre { get; set; }
+        [Required(ErrorMessage = "Por favor proporcionar una Calle con su número de casa.")]
         public string Calle { get; set; }
+        [Required(ErrorMessage = "Por favor proporcionar una Ciudad.")]
         public string Ciudad { get; set; }
     }
 }
