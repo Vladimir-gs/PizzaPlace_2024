c71ac34a68307f4ddfb39b298229aa71e92b777f
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-01-31 14:36:01 -0400

    Agregamos nuestra clase principal

---
+++ PizzaPlace_2024/Models/Pizza.cs
@@ -0,0 +1,27 @@
+﻿namespace PizzaPlace_2024.Models
+{
+    
+    public enum Especias
+    {
+        Ninguna,
+        Condimentado,
+        Picante
+    }
+
+    public class Pizza
+    {
+        public Pizza(int id, string nombre, decimal price,
+        Especias especias)
+        {
+            this.Id = id;
+            this.Nombre = nombre;
+            this.Precio = price;
+            this.Especias = especias;
+        }
+        public int Id { get; }
+        public string Nombre { get; }
+        public decimal Precio { get; }
+        public Especias Especias { get; }
+    }
+    
+}
