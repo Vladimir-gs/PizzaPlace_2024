c24b02ae23a358a645c988d1c96dfcd9f4c8c061
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-01-31 14:46:54 -0400

    Agregamos la clase depuracion

---
+++ PizzaPlace_2024/Models/Depuracion.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace PizzaPlace_2024.Models
+{
+    public static class Depuracion
+    {
+        private static JsonSerializerOptions options = new
+        JsonSerializerOptions
+        { WriteIndented = true };
+        public static string ToJson(this object obj)
+        => JsonSerializer.Serialize(obj, options);
+    }
+}
