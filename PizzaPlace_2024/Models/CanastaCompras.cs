6077ddd11529013c287b6948ba4286c88e339255
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-01-31 14:41:07 -0400

    Agregamos nuestra clase canastacompras

---
+++ PizzaPlace_2024/Models/CanastaCompras.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaPlace_2024.Models
+{
+    public class CanastaCompras
+    {
+        public Clientes Clientes { get; set; } = new Clientes();
+        public List<int> Otros { get; set; } = new List<int>();
+        public bool Pagado { get; set; }
+    }
+}
