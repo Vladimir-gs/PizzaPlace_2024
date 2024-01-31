c0b44a2d49c3015fc7c3584f826105883d2a0fb3
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-01-31 14:55:16 -0400

    Agregamos nuevos metodos a nuestra clase CanastaCOmpras

--- PizzaPlace_2024/Models/CanastaCompras.cs
+++ PizzaPlace_2024/Models/CanastaCompras.cs
@@ -4,6 +4,10 @@
     {
         public Clientes Clientes { get; set; } = new Clientes();
         public List<int> Otros { get; set; } = new List<int>();
+        public List<int> Pedidos { get; set; } = new List<int>();
         public bool Pagado { get; set; }
+        public void Add(int pizzaId) => Pedidos.Add(pizzaId);
+        public void RemoveAt(int pos) => Pedidos.RemoveAt(pos);
+
     }
 }
